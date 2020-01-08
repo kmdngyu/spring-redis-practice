a82ab0885cdb2ceec5f6d4e67283459dcf9085d0
kdg <kmdngyu@gmail.com>
2020-01-08 23:05:22 +0900

    Initial commit

---
+++ src/main/java/com/example/springredispractice/SpringRedisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springredispractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springredispractice/SpringRedisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springredispractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
